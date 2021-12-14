a4d595df26999ea34da5467a88fc96bc9e551cc4
Parth Brahmbhatt <parthb7999@gmail.com>
2021-12-14 18:14:10 +0530

    changeing

---
+++ parth1.R
@@ -0,0 +1,60 @@
+name <-"Parth123"
+num1 <- 12
+num2 <- 20
+text1<- text2<-"Banana" 
+A=-101L
+class(A)
+B=52.2
+A+B
+C<-as.complex(A)
+C
+class(C)
+median(1,2,3,4,5,6,7)
+q='parth R programminh
+language,
+poweful'
+cat(q)
+nchar(q)
+grepl("L",q)
+13<2
+12!=1
+11%%2
+TRUE | FALSE
+cities<-c('london','mumbai','newyork')
+cities
+country<-c('india','US','UK')
+country
+num1<-c(10,20,30,40)
+num1
+sequ_new<-1:100
+sequ_new
+seq1<- 1:5
+length(seq1)
+sort(cities)
+
+country[3]
+cities[-1]
+newnum1<-seq(from=0,to=50,by=5)
+newnum1
+fruits<-list('fig','cherry','lemon')
+fruits[2]
+fruits[1]<-'mangoes'
+fruits
+length(fruits)
+"fig"%in% fruits
+'UK'%in% country
+append(fruits,'pineapple')
+M<-matrix(c(1,2,3,4,5,6,7,8),nrow=4,ncol=2)
+M
+M[3,1]
+M[,2]
+M<-rbind(M,c(66,77))
+M
+M[-c(1),-c(2)]<-0
+M
+
+one_dim<-c(1:35)
+one_dim
+multidim<-array(one_dim,dim=c(5,4,4))
+multidim
+multidim[5,2,4]
